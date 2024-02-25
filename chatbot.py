313486236b2b70851b679488dbc23c1ace6124e6
Carrie Gan <111520726+carrgann@users.noreply.github.com>
2024-02-24 19:01:16 -0500

    simplebot

--- chatbot.py
+++ chatbot.py
@@ -8,14 +8,18 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
-# Load, chunk and index the contents of the blog.
-def load_sicknesses(filename):
-    with open(filename, 'r') as file:
-        data = json.load(file)
-    return data
+OPENAI_API_KEY = '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'
 
-sicknesses = load_sicknesses('SymptomsOutput.json')
-docs = sicknesses
+# Load, chunk and index the contents of the blog.
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
